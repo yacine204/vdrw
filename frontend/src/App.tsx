eceb2be02833392881f51914ab9f17fc9a0aaa06
Yacine <yacine204@gmail.com>
2026-03-22 14:05:27 +0100

    canvas fix testing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
@@ -9,6 +9,19 @@ import Canvas from "./pages/game_interface/canvas";
 import Vdrw from "./pages/game_interface/vdrw";
 import Host from "./pages/host";
 import Menu from "./pages/menu";
+
+function StaticCanvasPreview() {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  return (
+    <Canvas
+      canvasRef={canvasRef}
+      color="#000000"
+      size={4}
+      sendStroke={() => {}}
+      canDraw={false}
+    />
+  );
+}
 
 function App() {
   const [user, setUser] = useState<any>(null);
@@ -32,7 +45,7 @@ function App() {
       <Route path="/signup" element={<Signup></Signup>}></Route>
       <Route path="/join" element={<Join user={user}></Join>}></Route>
       <Route path="/chat" element={<Chat user={user}></Chat>}></Route>
-      <Route path="/canvas" element={<Canvas></Canvas>}></Route>
+      <Route path="/canvas" element={<StaticCanvasPreview />}></Route>
       <Route path='/vdrw' element={<Vdrw user={user}></Vdrw>}></Route>
       <Route path="/host" element={<Host user={user}></Host>}></Route>
       <Route path='menu' element = {<Menu user={user}></Menu>}></Route>
